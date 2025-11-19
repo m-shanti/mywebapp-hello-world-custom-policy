644db074c7da0a68f2c8911b6ff2fc75c0d26881
10001104 <maciejw@atena.pl>
2025-11-19 18:08:11 +0100

    draft for OBO

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,20 +2,36 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using mywebapp.Models;
+using Microsoft.Identity.Client;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
 
 namespace mywebapp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IOptions<OpenIdConnectOptions> _oidcOptions;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IOptions<OpenIdConnectOptions> oidcOptions)
     {
         _logger = logger;
+        _oidcOptions = oidcOptions;
     }
 
     public IActionResult Index()
     {
+        IConfidentialClientApplication msalClient = ConfidentialClientApplicationBuilder.Create(_oidcOptions.Value.ClientId)
+                    .WithClientSecret(_oidcOptions.Value.ClientSecret)
+                    .WithAuthority(new Uri(_oidcOptions.Value.Authority))
+                    .Build();
+
+        msalClient.AddInMemoryTokenCache();
+        msalClient.AcquireTokenForClient(new string[] { "https://graph.microsoft.com/.default" }).WithExtraQueryParameters("OnBehalfOf=user456").ExecuteAsync();
+
+        //AuthenticationResult msalAuthenticationResult = await msalClient.AcquireTokenForClient(new string[] { "https://graph.microsoft.com/.default" }).ExecuteAsync();
+
         return View();
     }
 
